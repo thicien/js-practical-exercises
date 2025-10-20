36ec895a9d9173371c41a1e2cf9f88c28b04f104
mugisha thicien <mugishathicien04@gmail.com>
2025-10-20 17:49:13 +0200

    updated

--- odd-squares.js
+++ odd-squares.js
@@ -1,10 +1,11 @@
 function oddSquares(arr) {
-    if (!Array.isArray(arr)) {
-        throw new TypeError("this must only have number only");
-    }
-    return arr
-        .filter(num => typeof num && num % 2 !== 0)
-        .map(num => num * num);
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Input must be an array of numbers");
+  }
+
+  return arr
+    .filter(num => typeof num === "number" && num % 2 !== 0) 
+    .map(num => num * num); 
 }
 console.log(oddSquares(4, 5, 7, 2, 9));
 console.log(oddSquares(45, 64, 31, 97, 25));
