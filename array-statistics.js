8dfc2b8ee58a6be109f9599b2f2a5aedef26be22
mugisha thicien <mugishathicien04@gmail.com>
2025-10-20 17:08:06 +0200

    sum, min and max

--- array-statistics.js
+++ array-statistics.js
@@ -1,3 +1,14 @@
-const coke = {
-    
+function arrayStats(arr) {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return { sum: 0, average: 0, min: 0, max: 0 };
+  }
+
+  const sum = arr.reduce((acc, num) => acc + num, 0);
+  const average = parseFloat((sum / arr.length).toFixed(2));
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return { sum, average, min, max };
 }
+console.log(arrayStats([1, 2, 3, 4, 5]));
+console.log(arrayStats([20, 5, -10, 0]));
