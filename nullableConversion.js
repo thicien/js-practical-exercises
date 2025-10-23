796bfd7b6692f774b0695aa11407d0a4a5c16ca9
mugisha thicien <mugishathicien04@gmail.com>
2025-10-23 13:49:51 +0200

    updated

--- nullableConversion.js
+++ nullableConversion.js
@@ -1,5 +1,5 @@
 function convertNullableVallues(obj) {
-    for (const key in obj) {
+    for (let key in obj) {
         if (obj[key] === null) {
             obj[key] === 0;
         } else if (obj[key] === undefined) {
