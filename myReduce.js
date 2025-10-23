8f47e2dbf7016e5fc0cdea65f0f547461d293337
mugisha thicien <mugishathicien04@gmail.com>
2025-10-23 14:51:14 +0200

    This problem has solved

--- myReduce.js
+++ myReduce.js
@@ -2,5 +2,16 @@ Array.prototype.myReduce = function (callbackFn) {
     let accumulator = initialValue;
     let startIndex = 0;
 
-    if
+    if (accumulator === undefined) {
+        accumulator = this[0];
+        startIndex = 1;
+    }
+    for (let i = 0; i < this.length; i++) {
+        accumulator = callbackFn(accumulator, this[i], i, this);  
+    }
+    return accumulator;
 }
+
+const numbers = [1, 2, 3, 4, 5];
+const reduced = numbers.myReduce((acc, num) => acc + num, 0);
+console.log('myReduce', reduced);
